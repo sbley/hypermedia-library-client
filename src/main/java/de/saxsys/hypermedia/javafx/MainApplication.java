41f8e530d78946c224086dfc23b359b0edf41db0
Alexander Casall <alexander.casall@saxsys.de>
2014-10-30 19:38:02 +0100

    Buttons fix

--- src/main/java/de/saxsys/hypermedia/javafx/MainApplication.java
+++ src/main/java/de/saxsys/hypermedia/javafx/MainApplication.java
@@ -83,8 +83,7 @@ public class MainApplication implements FxmlView {
         bookList.setCellFactory(p -> new BookCell());
         listProgress.setVisible(false);
         detailProgress.setVisible(false);
-        bookList.getSelectionModel()
-                .selectedItemProperty()
+        bookList.getSelectionModel().selectedItemProperty()
                 .addListener((ChangeListener<Book>) (arg0, oldBook, newBook) -> {
                     if (newBook != null) {
                         requestBookDisplay(newBook);
@@ -96,8 +95,7 @@ public class MainApplication implements FxmlView {
             if (newBook != null) {
                 titleLabel.setText(newBook.getTitle() == null ? "" : newBook.getTitle());
                 authorLabel.setText(newBook.getAuthor() == null ? "" : newBook.getAuthor());
-                descriptionLabel.setText(newBook.getDesc() == null ? "No Description"
-                        : newBook.getDesc());
+                descriptionLabel.setText(newBook.getDesc() == null ? "No Description" : newBook.getDesc());
                 returnName.setText(newBook.getBorrower() == null ? "Error" : "Lent to member: "
                         + newBook.getBorrower().toString());
                 lendTextField.setVisible(!newBook.isLent());
@@ -143,15 +141,14 @@ public class MainApplication implements FxmlView {
     @FXML
     void searchButtonPressed(ActionEvent event) {
         listProgress.setVisible(true);
-        new Async(
-                () -> {
-                    try {
-                        List<Book> search = bean.search(searchTextField.getText());
-                        Platform.runLater(() -> bookList.setItems(FXCollections.observableArrayList(search)));
-                    } finally {
-                        Platform.runLater(() -> listProgress.setVisible(false));
-                    }
-                }).start();
+        new Async(() -> {
+            try {
+                List<Book> search = bean.search(searchTextField.getText());
+                Platform.runLater(() -> bookList.setItems(FXCollections.observableArrayList(search)));
+            } finally {
+                Platform.runLater(() -> listProgress.setVisible(false));
+            }
+        }).start();
     }
 
     private void requestBookDisplay(Book book) {
