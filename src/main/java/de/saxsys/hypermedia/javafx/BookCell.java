ffb3f9636b98e67ee13b2b9fee00ec9b51735d91
Stefan Bley <stefan.bley@saxsys.de>
2014-10-31 18:42:05 +0100

    bugfix in cell renderer

--- src/main/java/de/saxsys/hypermedia/javafx/BookCell.java
+++ src/main/java/de/saxsys/hypermedia/javafx/BookCell.java
@@ -4,11 +4,14 @@ import javafx.scene.control.ListCell;
 import de.saxsys.hypermedia.javafx.service.Book;
 
 public class BookCell extends ListCell<Book> {
+
     @Override
-    protected void updateItem(Book arg0, boolean arg1) {
-        super.updateItem(arg0, arg1);
-        if (arg0 != null) {
-            setText(arg0.getTitle() + " by " + arg0.getAuthor());
+    protected void updateItem(Book item, boolean empty) {
+        super.updateItem(item, empty);
+        if (item != null) {
+            setText(item.getTitle() + " by " + item.getAuthor());
+        } else {
+            setText("");
         }
     }
 }
